b1ffe63a9b1d97d44095e5ad094bb071e0f0c620
Cong Danh Nguyen <danhcong.nguyen@dxc.com>
2023-10-12 15:15:12 +0700

    Add Try Except To improve UX

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 import ChangeName as CN
-
-folder_name = input("Type your folder you want to change \n")
-file_list = CN.ListFiles(folder_name)
-Folder_1 = CN.FileList(folder_name,file_list)
-new_name=[]
-while (len(new_name)<len(Folder_1.file_list)):
-    new_name.append(input("Type the new name :\n"))
-if (Folder_1.check_namelist(new_name)):
-    Folder_1.change_filename(new_name)
+try:
+    folder_name = input("Type your folder you want to change \n")
+    file_list = CN.ListFiles(folder_name)
+    Folder_1 = CN.FileList(folder_name,file_list)
+    new_name=[]
+    i=0
+    while (len(new_name)<len(Folder_1.file_list)):
+        print("OldName: "+Folder_1.file_list[i])
+        new_name.append(input("Type the new name : "))
+        print(Folder_1.file_list[i]+" will change to "+new_name[i])
+        i=i+1
+    if (Folder_1.check_namelist(new_name)):
+        Folder_1.change_filename(new_name)
+except:
+    print("Something went wrong please try again")
